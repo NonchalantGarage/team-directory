3b3f11b3c412a98ada64f9329745d70c551d49a7
Curtis <hong.curtis@gmail.com>
2022-01-15 10:04:58 -0800

    updated all inquiries for employee, manager, intern and a prompt for a method to continue or end the team assembly

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // const Manager = require('./lib/Manager');
 const inquirer = require('inquirer');
 
-const promptUser = () =>{
+const promptManager = () =>{
     return inquirer
         .prompt([
             {
@@ -21,14 +21,14 @@ const promptUser = () =>{
                 }
             },  
             {
-                name: 'Id', 
+                name: 'ID', 
                 type: 'input',
-                message: "Enter your manager's id",
+                message: "Enter your manager's ID",
                 validate: idInput =>{
                     if(idInput){
                         return true;
                     } else {
-                        console.log("Please enter a valid id!'");
+                        console.log("Please enter a valid ID!'");
                         return false;
                     }
                 }
@@ -49,12 +49,126 @@ const promptUser = () =>{
             {
                 name: 'Office Number', 
                 type: 'input',
-                message: "Enter your manager's officeNumber",
+                message: "Enter your manager's office number",
                 validate: officeNumberInput =>{
                     if(officeNumberInput){
                         return true;
                     } else {
-                        console.log("Please enter a valid Office Number!'");
+                        console.log("Please enter a valid office number!'");
+                        return false;
+                    }
+                },
+         },
+    ]);
+}
+const promptEngineer = () =>{
+    return inquirer
+        .prompt([
+            {
+                name: 'Name', 
+                type: 'input',
+                message: "Enter your engineer's name",
+                validate: nameInput =>{
+                    if(nameInput){
+                        return true;
+                    } else {
+                        console.log("Please enter your engineer's name!'");
+                        return false;
+                    }
+                }
+            },  
+            {
+                name: 'ID', 
+                type: 'input',
+                message: "Enter your engineer's ID",
+                validate: idInput =>{
+                    if(idInput){
+                        return true;
+                    } else {
+                        console.log("Please enter a valid ID!'");
+                        return false;
+                    }
+                }
+            },
+            {
+            name: 'Email', 
+            type: 'input',
+            message: "Enter your engineer's email",
+            validate: emailInput =>{
+                if(emailInput){
+                    return true;
+                } else {
+                    console.log("Please enter a valid email!'");
+                    return false;
+                }
+            }
+        },
+            {
+                name: 'GitHub', 
+                type: 'input',
+                message: "Enter your engineer's GitHub username",
+                validate: GitHub =>{
+                    if(GitHub){
+                        return true;
+                    } else {
+                        console.log("Please enter a valid GitHub username!'");
+                        return false;
+                    }
+                },
+         },
+    ]);
+}
+const promptIntern = () =>{
+    return inquirer
+        .prompt([
+            {
+                name: 'Name', 
+                type: 'input',
+                message: "Enter your intern's name",
+                validate: nameInput =>{
+                    if(nameInput){
+                        return true;
+                    } else {
+                        console.log("Please enter your intern's name!'");
+                        return false;
+                    }
+                }
+            },  
+            {
+                name: 'ID', 
+                type: 'input',
+                message: "Enter your intern's ID",
+                validate: idInput =>{
+                    if(idInput){
+                        return true;
+                    } else {
+                        console.log("Please enter a valid ID!'");
+                        return false;
+                    }
+                }
+            },
+            {
+            name: 'Email', 
+            type: 'input',
+            message: "Enter your intern's email",
+            validate: emailInput =>{
+                if(emailInput){
+                    return true;
+                } else {
+                    console.log("Please enter a valid email!'");
+                    return false;
+                }
+            }
+        },
+            {
+                name: 'GitHub', 
+                type: 'input',
+                message: "Enter your intern's school name",
+                validate: school =>{
+                    if(school){
+                        return true;
+                    } else {
+                        console.log("Please enter a valid school name!'");
                         return false;
                     }
                 },
@@ -62,5 +176,24 @@ const promptUser = () =>{
     ]);
 }
 
+const promptContinue = () =>{
+    return inquirer
+        .prompt([
+            {
+                name: 'EngineerOrIntern', 
+                type: 'checkbox',
+                choices: ['Engineer', 'Intern', 'Team Assembled'],
+                message: "Please choose to add an Engineer, Intern or choose Team Assembled if you are finished",
+            }
+        ]);
+}
 
-promptUser().then(teamArr => console.log(teamArr))
+
+promptManager()
+    .then(teamArr => console.log(teamArr))
+    .then(promptContinue);
+
+// prompt user to enter Manager info
+
+
+// then(promptEngineer or Intern)
